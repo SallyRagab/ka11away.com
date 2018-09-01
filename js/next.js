f5009c669697665ce209ed69c4a9286236f66d5f
Sally Ragab <sallyragab31@gmail.com>
2018-09-01 15:55:52 +0200

    Add files via upload

--- js/next.js
+++ js/next.js
@@ -1,1 +1,16 @@
-// GOAL: To imitate terminal to the fullest extent possible
+
+// TODO A wall of available commands
+
+$(document).ready (function() {
+
+  let $terminal = $ ('#terminal');
+  let historyDisplayedIndex = -1;
+  let lastDisplayedIndex = -1;
+
+  let termHistory = [];
+
+  // Text options
+  let wakeUp = "Wake up, Neo...",
+  knock = "Knock, knock, Neo...",
+  
+}
